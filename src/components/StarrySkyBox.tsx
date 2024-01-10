22ad6d2a42074a61c345b45deab7b9ed48fed82c
Eadweard <804875728@qq.com>
2024-01-10 22:19:20 +0800

    初始化完成

---
+++ src/components/StarrySkyBox.tsx
@@ -0,0 +1,12 @@
+import * as THREE from 'three'
+import React, { useEffect, useRef } from 'react'
+
+const StarrySkyBox = () => {
+  
+  return (
+    <div className="w-screen h-screen bg-cyan-200">
+      111
+    </div>
+  )
+}
+export default StarrySkyBox
