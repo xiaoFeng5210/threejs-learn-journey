ac6d8fc8aede443eecc75d8c0313bd5d9a74dcc4
Eadweard <804875728@qq.com>
2024-01-28 17:59:20 +0800

    finish

--- src/components/WebglTest01.tsx
+++ src/components/WebglTest01.tsx
@@ -2,8 +2,26 @@ import * as Aurora from 'aurora-utils'
 
 const WebglTest = () => {
   console.log(Aurora.webglUtils)
+
+  const vertexShader = `
+  attribute vec4 a_Position;
+  attribute float a_PointSize;
+  void main() {
+    gl_Position = a_Position;
+    gl_PointSize = a_PointSize;
+  }
+  `
+
+  const fragmentShader = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+    }
+  `
+
   return (
-    <></>
+    <div className='w-full h-full bg-slate-400'>
+      <canvas id='canvas'></canvas>
+    </div>
   )
 }
 export default WebglTest
