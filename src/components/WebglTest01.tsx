5e890f552ee519aaf1f811a886a74ad2a10c20bf
Eadweard <804875728@qq.com>
2024-01-28 18:10:37 +0800

    draw

--- src/components/WebglTest01.tsx
+++ src/components/WebglTest01.tsx
@@ -1,4 +1,5 @@
 import * as Aurora from 'aurora-utils'
+import { useEffect } from 'react'
 
 const WebglTest = () => {
   console.log(Aurora.webglUtils)
@@ -17,6 +18,33 @@ const WebglTest = () => {
       gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
     }
   `
+
+  function draw() {
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+
+    const gl = canvas.getContext('webgl') as WebGLRenderingContext
+    Aurora.webglUtils.initShaders(gl, vertexShader, fragmentShader)
+
+    const a_Position = gl.getAttribLocation((gl as any).program, 'a_Position')
+    const a_PointSize = gl.getAttribLocation((gl as any).program, 'a_PointSize')
+
+    gl.vertexAttrib3f(a_Position, 0.1, 0.0, 0.0)
+    gl.vertexAttrib1f(a_PointSize, 100.0)
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    gl.drawArrays(gl.POINTS, 0, 1)
+  }
+
+  useEffect(() => {
+    draw()
+  }, [])
+
+
+
 
   return (
     <div className='w-full h-full bg-slate-400'>
