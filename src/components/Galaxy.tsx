92728fea249a7e8cd9caf0538c136a27daf32168
Eadweard <aurora.coder.zqf@gmail.com>
2024-04-15 22:46:45 +0800

    add mesh

--- src/components/Galaxy.tsx
+++ src/components/Galaxy.tsx
@@ -1,20 +1,26 @@
 import React, {useEffect, useRef} from "react";
 import { Canvas } from '@react-three/fiber'
+import { extend } from '@react-three/fiber'
+import { OrbitControls, TransformControls } from 'three-stdlib'
+import {CameraControls} from "@react-three/drei"
+extend({ OrbitControls, TransformControls })
 
 const Galaxy = () => {
   const canvasRef = useRef(null)
+	const camera = { fov: 55, near: 0.1, far: 1000, position: [2, 3, 5] }
 	useEffect(() => {
 		
 	}, []);
 	return (
 		<div ref={canvasRef} className="w-screen h-screen text-amber-700 text-3xl">
-			<Canvas>
-				<ambientLight intensity={0.1}/>
-				<directionalLight color="red" position={[0, 0, 5]}/>
+			<Canvas shadows camera={camera as any}>
+				<ambientLight intensity={0.5} color="white"/>
+				<directionalLight position={[3, 1, 5]}/>
 				<mesh>
 					<boxGeometry/>
-					<meshStandardMaterial/>
+					<meshPhongMaterial color="green"/>
 				</mesh>
+				<CameraControls makeDefault/>
 			</Canvas>
 		</div>
 	)
