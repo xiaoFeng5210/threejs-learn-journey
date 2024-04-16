43348ac9aa0473ce8989ce15f4116938f53e9623
Eadweard <aurora.coder.zqf@gmail.com>
2024-04-16 13:38:25 +0800

    汽车动画🚗

--- src/components/3d/CoolCar.tsx
+++ src/components/3d/CoolCar.tsx
@@ -1,14 +1,22 @@
 import {useGLTF} from '@react-three/drei'
 // @ts-ignore
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import {useFrame} from "@react-three/fiber";
 
 const CoolCar = () => {
-	const { nodes, materials, scene } = useGLTF( '/gltf/car/scene.gltf')
+	const {nodes, materials, scene} = useGLTF('/gltf/car/scene.gltf')
 	console.log(nodes)
 	console.log(materials)
 	console.log(scene)
+
+	useFrame(({clock}) => {
+		const elapsedTime = clock.getElapsedTime()
+		const step = Math.sin(elapsedTime) * 10
+		scene.position.set(0, 0, step)
+	})
 	return (
 		<primitive scale={[0.01, 0.01, 0.01]} object={scene}/>
 	)
 }
+
 export default CoolCar
--- src/components/Galaxy.tsx
+++ src/components/Galaxy.tsx
@@ -1,5 +1,5 @@
 import {Suspense, useEffect, useRef} from "react";
-import {Canvas} from '@react-three/fiber'
+import {Canvas, useFrame} from '@react-three/fiber'
 import {Stats, OrbitControls} from '@react-three/drei'
 import * as Three from 'three'
 // @ts-ignore
@@ -8,8 +8,8 @@ import CoolCar from "./3d/CoolCar";
 const Galaxy = () => {
 	const canvasRef = useRef(null)
 	const camera = {fov: 55, near: 0.1, far: 1000, position: [16, 12, -16]}
-	useEffect(() => {
-	}, []);
+	const planeMeshRef = useRef(null)
+
 	return (
 		<div ref={canvasRef} className="w-screen h-screen text-amber-700 text-3xl">
 			<Canvas camera={camera as any}>
@@ -19,7 +19,7 @@ const Galaxy = () => {
 					<spotLight position={[3, 3, 0]} intensity={100} penumbra={0.4} angle={1}/>
 					<hemisphereLight args={[0xddeeff, 0x0f0e0d, 0.02]} intensity={50}/>
 					<CoolCar/>
-					<mesh rotation={[Math.PI / 2, 0, 0]}>
+					<mesh ref={planeMeshRef} rotation={[Math.PI / 2, 0, 0]}>
 						<planeGeometry args={[10, 1000]}/>
 						<meshBasicMaterial color="#10E8E8" side={Three.DoubleSide}/>
 					</mesh>
