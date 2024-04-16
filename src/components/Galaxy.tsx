28b04f448c1f55ed760196ca4c668accc49f26be
Eadweard <aurora.coder.zqf@gmail.com>
2024-04-16 11:41:45 +0800

    酷酷的小车

--- src/components/Galaxy.tsx
+++ src/components/Galaxy.tsx
@@ -1,17 +1,14 @@
-import React, {Suspense, useEffect, useRef} from "react";
+import {Suspense, useEffect, useRef} from "react";
 import {Canvas} from '@react-three/fiber'
 import {Stats, OrbitControls} from '@react-three/drei'
 import * as Three from 'three'
-
 // @ts-ignore
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 import CoolCar from "./3d/CoolCar";
 
 const Galaxy = () => {
 	const canvasRef = useRef(null)
 	const camera = {fov: 55, near: 0.1, far: 1000, position: [16, 12, -16]}
 	useEffect(() => {
-
 	}, []);
 	return (
 		<div ref={canvasRef} className="w-screen h-screen text-amber-700 text-3xl">
@@ -23,8 +20,8 @@ const Galaxy = () => {
 					<hemisphereLight args={[0xddeeff, 0x0f0e0d, 0.02]} intensity={50}/>
 					<CoolCar/>
 					<mesh rotation={[Math.PI / 2, 0, 0]}>
-						<planeGeometry args={[8, 1000]}/>
-						<meshBasicMaterial color={0xffff00} side={Three.DoubleSide}/>
+						<planeGeometry args={[10, 1000]}/>
+						<meshBasicMaterial color="#10E8E8" side={Three.DoubleSide}/>
 					</mesh>
 					<OrbitControls/>
 					<Stats/>
